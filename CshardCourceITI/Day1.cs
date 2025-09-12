c44fdb1264aeebeb179eb8d56ed82b9b8106cbc3
aammo <aabo66220@gmail.com>
2025-09-12 18:48:59 +0300

    Add project files.

---
+++ CshardCourceITI/Day1.cs
@@ -0,0 +1,91 @@
+﻿
+namespace D1CSharp
+{
+    internal class Day1
+    {
+        public static void ASCII(char c)
+        {
+            Console.WriteLine("Character: " + c);
+            Console.WriteLine("ASCII code: " + (int)c);
+        }
+        public static void UnASCII(int c)
+        {
+            Console.WriteLine("ASCII code: " + c);
+            Console.WriteLine("Character: " + (char)c);
+        }
+        public static bool is_Even(int i)
+        {
+            return(i%2==0);
+        }
+        public static void Calc(int a, int b)
+        {
+            char c;
+            Console.Write("Enter op (+ , - , * , /): ");
+            c = Convert.ToChar(Console.Read());
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine(a + b);
+                    break;
+
+                case '-':
+                    Console.WriteLine(a - b);
+                    break;
+
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+
+                case '/':
+                    Console.WriteLine(a / b);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+        }
+        public static void grade(int d)
+        {
+            if (d >= 90)
+            {
+                Console.WriteLine("Student got A");
+            }
+            else if (d >= 80 && d < 90)
+            {
+                Console.WriteLine("Student got B");
+            }
+            else if (d >= 70 && d < 80)
+            {
+                Console.WriteLine("Student got C");
+            }
+            else if (d >= 60 && d < 70)
+            {
+                Console.WriteLine("Student got D");
+            }
+            else
+            {
+                Console.WriteLine("Student has failed.");
+            }
+        }
+        public static void multiTable()
+        {
+            int num = 0;
+            for(int i = 1; i < 13; i++)
+            {
+                Console.WriteLine("Table of: " + i);
+                Console.WriteLine("-----------------");
+                for (int j = 1; j < 13; j++)
+                {
+                    num = i * j;
+                    Console.WriteLine(i + " * " + j + " = " + num);
+                }
+                Console.WriteLine("\n-----------------");
+            }    
+        }
+        static void Main(string[] args)
+        {
+            Day2.Traks(3,5);
+        }
+    }
+}
---
+++ CshardCourceITI/Day2.cs
@@ -0,0 +1,72 @@
+﻿namespace D1CSharp
+{
+    public class Day2
+    {
+        public static void StuList(int num)
+        {
+            string[] stuarr = new string[num];
+            Console.WriteLine("Enter all student names:\n");
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Student " + (i + 1) + ": ");
+                stuarr[i] = (Console.ReadLine());
+            }
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Student " + (i + 1) + ": " + stuarr[i]);
+            }
+        }
+
+        public static void Traks(int track_num, int stu_num)
+        {
+            int[,] trackList = new int[track_num, stu_num];
+            float[] avg = new float[track_num];
+            float total = 0;
+            for (int i = 0; i < track_num; i++)
+            {
+                Console.WriteLine("Track " + (i + 1) + ": ");
+
+                for (int j = 0; j < stu_num; j++)
+                {
+                    trackList[i, j] = Convert.ToInt32(Console.ReadLine());
+                    total += trackList[i, j];
+                }
+                avg[i] = total / stu_num;
+                Console.WriteLine();
+            }
+            for (int i = 0; i < track_num; i++)
+            {
+                Console.WriteLine("Track " + (i + 1) + ": ");
+
+                for (int j = 0; j < stu_num; j++)
+                {
+                    Console.Write("\t");
+                    Console.Write(trackList[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            for (int j = 0; j < track_num; j++)
+            {
+                Console.WriteLine("AVG of Track " + (j + 1) + ": ");
+                Console.WriteLine(avg[j] + "\n");
+            }
+        }
+
+        public static void DateList()
+        {
+            int[][] time = new int[1][];
+            time[0] = new int[3];
+
+            time[0][0] = 22;
+            time[0][1] = 33;
+            time[0][2] = 11;
+
+            int hours = time[0][0];
+            int minutes = time[0][1];
+            int seconds = time[0][2];
+
+            Console.WriteLine($"{hours}H:{minutes}M:{seconds}S");
+        }
+    }
+}
